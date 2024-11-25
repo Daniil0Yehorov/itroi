93bb073492d05b6cbcdcf95fcbb793a86e23d034
Daniil0Yehorov <danvochdanvoch@gmail.com>
2024-11-25 17:33:08 +0100

    added web-interface for all user functions; next will be administrators

--- src/main/java/com/itroi/itroi/ItroiApplication.java
+++ src/main/java/com/itroi/itroi/ItroiApplication.java
@@ -1,5 +1,7 @@
 package com.itroi.itroi;
 
+import com.itroi.itroi.generated_models.Product;
+import com.itroi.itroi.generated_models.User;
 import com.itroi.itroi.serviceimpl.cartImplementation;
 import com.itroi.itroi.serviceimpl.productImplementation;
 import com.itroi.itroi.serviceimpl.userImplementation;
@@ -30,7 +32,61 @@ public class ItroiApplication {
             Endpoint.publish("http://localhost:8081/ws/products?wsdl", productService);
             Endpoint.publish("http://localhost:8081/ws/users?wsdl",userService);
             System.out.println("SOAP Web Services is running...");
+			Product product1 = new Product();
+			product1.setCategory("Dog Food");
+			product1.setCountInStock(50);
+			product1.setDescription("Premium dog food");
+			product1.setID(1);
+			product1.setName("Royal Canin");
+			product1.setPrice(30.0);
+			productService.addProduct(product1);
 
+			Product product2 = new Product();
+			product2.setCategory("Cat Food");
+			product2.setCountInStock(30);
+			product2.setDescription("Healthy cat food");
+			product2.setID(2);
+			product2.setName("Whiskas");
+			product2.setPrice(20.0);
+			productService.addProduct(product2);
+
+			Product product3 = new Product();
+			product3.setCategory("Bird Food");
+			product3.setCountInStock(15);
+			product3.setDescription("Nutrient-rich food for birds");
+			product3.setID(3);
+			product3.setName("Versele-Laga");
+			product3.setPrice(10.0);
+			productService.addProduct(product3);
+			User admin = new User();
+			admin.setEmail("admin@example.com");
+			admin.setID(userService.generateUniqueUserId());
+			admin.setLogin("admin");
+			admin.setName("Admin User");
+			admin.setPassword("admin123");
+			admin.setPhone("123-456-7890");
+			admin.setType("Admin");
+			userService.createUser(admin);
+
+			User user1 = new User();
+			user1.setEmail("user1@example.com");
+			user1.setID(userService.generateUniqueUserId());
+			user1.setLogin("user1");
+			user1.setName("User One");
+			user1.setPassword("password1");
+			user1.setPhone("321-654-0987");
+			user1.setType("User");
+			userService.createUser(user1);
+
+			User user2 = new User();
+			user2.setEmail("user2@example.com");
+			user2.setID(userService.generateUniqueUserId());
+			user2.setLogin("user2");
+			user2.setName("User Two");
+			user2.setPassword("password2");
+			user2.setPhone("987-654-3210");
+			user2.setType("User");
+			userService.createUser(user2);
 		};
 	}
 }
